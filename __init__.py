9c2a6cd79bb5fa4ab812e45200035df82ef8e971
dcolens <dcolens@cisco.com>
2022-08-18 09:13:27 +0000

    "/var/bdb/nas/tasks_dev/sleep" updated by dcolens

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals, absolute_import, print_function
+from time import sleep
+
+def task(env, duration=1, size=1):
+    """
+    sleeps `duration`  second and returns `size` times "x", used to test timeouts and large outputs.
+    """
+    #sleep duration seconds
+    sleep(duration)
+    
+    #return size times "x"
+    return "x" * size
